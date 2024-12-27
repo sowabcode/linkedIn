f9298694ebbe7f460a3bbf7c096ddda7add19a39
abdoulayebademba <sowabdoulaye05@gmail.com>
2024-12-27 07:14:28 +0000

    chore: set up backend & frontend using Spring initializr & Vite framework

---
+++ backend/src/main/java/com/sowab/linkedin/LinkedinApplication.java
@@ -0,0 +1,13 @@
+package com.sowab.linkedin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkedinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkedinApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sowab/linkedin/LinkedinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sowab.linkedin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkedinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
